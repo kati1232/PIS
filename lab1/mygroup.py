2b585db35a9c50d30e3704ad4effe83156ccd8a6
kati1232 <katixarlamova09@gmail.com>
2026-01-21 20:53:33 +0300

    1 Лабораторная работа

---
+++ lab1/mygroup.py
@@ -0,0 +1,43 @@
+groupmates = [
+    {
+        "name": "Александр",
+        "surname": "Иванов",
+        "exams": ["Информатика", "ЭЭиС", "Web"],
+        "marks": [4, 3, 5]
+    },
+    {
+        "name": "Иван",
+        "surname": "Петров",
+        "exams": ["История", "АиГ", "КТП"],
+        "marks": [4, 4, 4]
+    },
+    {
+        "name": "Кирилл",
+        "surname": "Смирнов",
+        "exams": ["Философия", "ИС", "КТП"],
+        "marks": [5, 5, 5]
+    }
+]
+
+def print_students(students):
+    print(u"Имя".ljust(15), u"Фамилия".ljust(10), u"Экзамены".ljust(30), u"Оценки".ljust(20))
+    for student in students:
+        print(student["name"].ljust(15), student["surname"].ljust(10), str(student["exams"]).ljust(30),
+              str(student["marks"]).ljust(20))
+
+def get_students_above_average(students, min_avg):
+    result = []
+    for s in students:
+        avg = sum(s["marks"]) / len(s["marks"])
+        if avg > min_avg:
+            result.append(s)
+    return result
+
+
+threshold = float(input("Введите минимальный средний балл: "))
+filtered = get_students_above_average(groupmates, threshold)
+
+if filtered:
+    print_students(filtered)
+else:
+    print("Нет студентов с таким средним баллом")
